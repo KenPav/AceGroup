6937a8f91f572e340539df305de2870a7359d7cb
KP <65498619+KenPav@users.noreply.github.com>
2023-06-08 14:08:46 -0400

    Add files via upload

--- ACE.js
+++ ACE.js
@@ -330,12 +330,29 @@
             fill(FirstColor);
             text("West",600*xAdj,255*yAdj);
             fill(BoxColor);
-            if(Front===0 && Back===1) {FrontBack=0;
-               else if(Front===1 && Back===0) {FrontBack=1;
-                  else if(Front===2 && Back===1) {FrontBack=0;
-                     else if(Front===1 && Back===2) {FrontBack=1;
-                        else if(Front===0 && Back===2) {FrontBack=0;
-                           else if(Front===2 && Back===0) {FrontBack=1;}}}}}}
+            if(Front===0 && Back===1) {
+               FrontBack=0;
+            } else { 
+               if(Front===1 && Back===0) {
+                  FrontBack=1;
+               } else { 
+                  if(Front===2 && Back===1) {
+                     FrontBack=0;
+                  } else {
+                     if(Front===1 && Back===2) {
+                        FrontBack=1;
+                     } else {
+                        if(Front===0 && Back===2) {
+                           FrontBack=0;
+                        } else {
+                           if(Front===2 && Back===0) {
+                              FrontBack=1;
+                        }
+                     }
+                  }
+               }
+            }
+         }
             rect(300*xAdj,300*yAdj,200*xAdj,70*yAdj);
             fill(FirstColor);
             text("Continue",400*xAdj,355*yAdj);
