9fcadc4a57d798142844e83ab0d0eed6a4bf6b6f
KP <65498619+KenPav@users.noreply.github.com>
2023-06-11 12:25:25 -0400

    Add files via upload

--- ACE.js
+++ ACE.js
@@ -755,9 +755,9 @@
                rect(50*xAdj,700*yAdj,300*xAdj,125*yAdj);
                rect(450*xAdj,700*yAdj,300*xAdj,125*yAdj);
                fill(FirstColor);
-               textSize(90);
-               text("Cancel",200*xAdj,850*yAdj);
-               text("Confirm",600*xAdj,850*yAdj);
+               textSize(80);
+               text("Cancel",200*xAdj,800*yAdj);
+               text("Confirm",600*xAdj,800*yAdj);
 
             }
 
